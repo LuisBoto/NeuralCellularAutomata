88917371c2770ebd3e38940e775b27290d5dcc83
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2022-12-28 11:24:20 +0100

    Added callable wasm from main javascript

---
+++ js/Main.js
@@ -0,0 +1,36 @@
+// Canvas & context
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext('2d', { alpha: false });
+let minimumResize = 1;
+let canvasWidth = 1920;
+let canvasHeight = 1080;
+
+function callWebAssemblyFunction(functionName) {
+    WebAssembly.instantiateStreaming(fetch('../lib/build/distributions/lib.wasm'), wasmImports)
+      .then(obj => {
+        wasmImports.instance = obj.instance;
+        obj.instance.exports[functionName]();
+      })
+      .catch(err => document.write(err));
+}
+
+function loop(){
+    //layer.update();
+    //layer.draw();
+}
+
+// Resize
+window.addEventListener('load', resize, false);
+function resize() {
+    console.log("Resize")
+    let resizeWidth = parseFloat(window.innerWidth*0.89 / canvas.width);
+    let resizeHeight = parseFloat(window.innerHeight*0.89 / canvas.height);
+
+    minimumResize = Math.min(resizeWidth, resizeHeight);
+    canvas.width = canvas.width*minimumResize;
+    canvas.height = canvas.height*minimumResize;
+
+    context.scale(minimumResize, minimumResize);
+}
+
+callWebAssemblyFunction("main");
