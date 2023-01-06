140df8c63650f91112d4cb2214ec8ec53b6865e2
Luis Boto <43352462+LuisBoto@users.noreply.github.com>
2023-01-06 16:50:36 +0100

    Totally updatable activation function

--- src/global/Configuration.js
+++ src/global/Configuration.js
@@ -8,9 +8,16 @@ let kernel = [
     [0.8,     -0.85,     0.8]
 ]
 
+let updateActivation = () => {
+    eval( "function activation(x) { " + activationFunctionBody + " }" )
+    window["activation"] = activation;
+};
+
 function updateConfiguration() {
     columnNumber = parseInt(document.getElementById("inputColumnNumber").value);
     rowNumber = parseInt(document.getElementById("inputRowNumber").value);
+    activationFunctionBody = document.getElementById("activationFunctionBody").value;
+    updateActivation();
 
     layer.initiate();
 }
@@ -18,8 +25,8 @@ function updateConfiguration() {
 function updateInputFieldsValues() {
     document.getElementById("inputColumnNumber").value = columnNumber;
     document.getElementById("inputRowNumber").value = rowNumber;
+    document.getElementById("activationFunctionBody").value = activationFunctionBody;
 }
 
 updateInputFieldsValues();
-
-eval( "function activation(x) { " + activationFunctionBody + " }" );
+updateActivation();
